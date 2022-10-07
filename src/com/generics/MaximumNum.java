4831078a1b231595eaa120c75ba5fe8e10ca2441
shadab <me.shadab19@gmail.com>
2022-10-07 19:40:26 +0530

    [Added- Getting Max Float Number]

--- src/com/generics/MaximumNum.java
+++ src/com/generics/MaximumNum.java
@@ -2,9 +2,9 @@ package com.generics;
 
 public class MaximumNum {
 
-	public static Integer maximumInteger(Integer num1, Integer num2, Integer num3) {
+	public static Float maximumFloat(Float num1, Float num2, Float num3) {
 
-		Integer max = num1;
+		Float max = num1;
 		if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
 			max = num1;
 		else if (num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
@@ -16,7 +16,7 @@ public class MaximumNum {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Maximum Number Among 3 will be :- " + maximumInteger(70, 40, 50));
+		System.out.println("Maximum Float Number Among 3 will be :- " + maximumFloat(70.6f, 40.7f, 50.8f));
 	}
 
 }
