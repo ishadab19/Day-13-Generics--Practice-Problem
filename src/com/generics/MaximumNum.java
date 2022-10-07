e72ee5117e594149186c8415af78b5cac8f9dcd2
shadab <me.shadab19@gmail.com>
2022-10-07 20:11:53 +0530

    [Added-Refactor all the 3 to one Generic Method]

--- src/com/generics/MaximumNum.java
+++ src/com/generics/MaximumNum.java
@@ -1,10 +1,11 @@
 package com.generics;
 
-public class MaximumNum {
+public class MaximumNum<T> {
+	T num1, num2, num3;
 
-	public static String maximumString(String num1, String num2, String num3) {
+	public static <T extends Comparable<T>> T maximumaAmong3(T num1, T num2, T num3) {
 
-		String max = num1;
+		T max = num1;
 		if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
 			max = num1;
 		else if (num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
@@ -16,7 +17,12 @@ public class MaximumNum {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Maximum String Among 3 will be :- " + maximumString("Papaya", "Apple", "Banana"));
+		System.out.println("The max Number Among 3 String numbers is:-\n "+maximumaAmong3("Orange", "Dragonfruit", "Banana"));
+		System.out.println("The max Number Among 3 Integer numbers is:-\n "+maximumaAmong3(14,22,27));
+		System.out.println("The max Number Among 3 Float numbers is:-\n "+maximumaAmong3(39.5f,96.98f,98.99f));
 	}
 
-}
+
+	}
+
+
