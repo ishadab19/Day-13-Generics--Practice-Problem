837868984a4326e60cac83d1732d74602191cb3e
shadab <me.shadab19@gmail.com>
2022-10-07 19:34:24 +0530

    [Added- Getting Max Int Number]

--- src/com/generics/MaximumNum.java
+++ src/com/generics/MaximumNum.java
@@ -2,8 +2,21 @@ package com.generics;
 
 public class MaximumNum {
 
+	public static Integer maximumInteger(Integer num1, Integer num2, Integer num3) {
+
+		Integer max = num1;
+		if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
+			max = num1;
+		else if (num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
+			max = num2;
+		else
+			max = num3;
+		return max;
+
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome To Generic Practice Problem");
+		System.out.println("Maximum Number Among 3 will be :- " + maximumInteger(70, 40, 50));
 	}
 
 }
